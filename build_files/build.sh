b899f213d0dafe411e6480518e3180e676f7ff4a
Bilgin Aksoy <bilginaksoy2003@yahoo.com>
2026-06-08 23:25:37 +0200

    feat: add nvidia docker

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,6 +5,11 @@ dnf5 install -y \
     /tmp/akmods-rpms/ublue-os/ublue-os-nvidia*.rpm \
     /tmp/akmods-rpms/kmods/kmod-nvidia*.rpm \
     /tmp/akmods-rpms/nvidia/*.rpm \
-    libva-nvidia-driver
+    libva-nvidia-driver \
+    nvidia-container-toolkit
 
 dnf5 clean all
+
+# Configure Docker to use the NVIDIA container runtime
+mkdir -p /etc/docker
+nvidia-ctk runtime configure --runtime=docker
