9a7787eee4ae8df414f8c65caa60696a21c29867
Bilgin Aksoy <bilginaksoy2003@yahoo.com>
2026-08-04 19:55:16 +0200

    feat: install kernel-devel before tuxedo-drivers and update service enabling

--- build_files/build-nvidia-open.sh
+++ build_files/build-nvidia-open.sh
@@ -24,10 +24,23 @@ dnf5 install -y \
 FEDORA_VER=$(rpm -E '%fedora')
 rpm --import "https://rpm.tuxedocomputers.com/fedora/${FEDORA_VER}/0x54840598.pub.asc"
 dnf5 config-manager addrepo --from-repofile="https://rpm.tuxedocomputers.com/fedora/tuxedo.repo"
-dnf5 install -y --exclude=tuxedo-control-center \
+
+# kernel-devel must be present before tuxedo-drivers installs so its DKMS
+# build has headers to build the module against.
+KERNEL_VERSION=$(rpm -q --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}' kernel-core)
+dnf5 install -y kernel-devel-${KERNEL_VERSION}
+
+dnf5 install -y \
+    tuxedo-control-center \
     tuxedo-drivers \
     tuxedo-tomte \
-    tuxedo-firmware-collection
+    tuxedo-firmware-collection \
+    nspr \
+    nss
+
+# tccd's own %post can't reliably enable services during a container build
+# (no running systemd), same issue worked around below for ublue-nvctk-cdi.
+systemctl enable --root=/ tccd.service tccd-sleep.service
 
 dnf5 clean all
 
